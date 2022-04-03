881cc9c9aae08153e476e13214bcfce08a0da100
Abirami2329 <96733813+Abirami2329@users.noreply.github.com>
2022-04-04 00:24:01 +0530

    Update sliderimage.js

--- task5/sliderimage.js
+++ task5/sliderimage.js
@@ -1,24 +1,30 @@
  let indexValue = 1;
  const img = document.querySelectorAll('img');
  showImg(indexValue);
- function btm_slide(slideNumber){showImg(indexValue = slideNumber);}
-      function side_slide(slideNumber){showImg(indexValue += slideNumber);}
+ function btm_slide(slideNumber){
+	 indexValue = slideNumber
+	 showImg(indexValue);
+ }
+ function side_slide(slideNumber){
+	 indexValue += slideNumber
+	 showImg(indexValue);
+ }
       function showImg(slideNumber){
         const slider = document.querySelectorAll('.btm-slides span');
         if(slideNumber > img.length){indexValue = 1}
         if(slideNumber < 1){indexValue = img.length}
-        for(let i = 0; i < img.length; i++){
-          img[i].style.display = "none";
+        for(let image of img){
+          image.style.display = "none";
         }
-        for(let i = 0; i < slider.length; i++){
-          slider[i].style.background = "rgba(255,255,255,0.1)";
+        for(let sliderValue of slider){
+          sliderValue.style.background = "rgba(255,255,255,0.1)";
         }
         img[indexValue-1].style.display = "block";
         slider[indexValue-1].style.background = "white";
-        previousButton(indexValue);
-        nextButton(indexValue);
+        previousButton();
+        nextButton();
       }
-      function previousButton(indexValue) {
+      function previousButton() {
 	if(indexValue === 1){
     	   element = document.getElementById("previous");
     	   element.style.display = "none";
@@ -27,7 +33,7 @@
     	   element.style.display = "block";
 	}
       }
-     function nextButton(indexValue) {
+     function nextButton() {
 	if(indexValue === img.length){
     	   element = document.getElementById("next");
     	   element.style.display = "none";
